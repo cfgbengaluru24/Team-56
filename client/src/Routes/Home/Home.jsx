44ffb9386f95d35f1ed0f1bea6899e9490287814
Darren <darren.veigas@gmail.com>
2024-07-28 08:34:02 +0530

    New Donor side

--- client/src/Routes/Home/Home.jsx
+++ client/src/Routes/Home/Home.jsx
@@ -3,15 +3,30 @@ import { useNavigate } from 'react-router-dom';
 import { Button, Box } from '@mui/material';
 import './Home.css'; // Import the CSS file for styling
 import About from './about'
+import { db } from '../../fbconfig';
 import { getAuth } from 'firebase/auth';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 export function Home() {
     const navigate = useNavigate();
     const [userEmail, setUserEmail] = useState('');
-
+    const [usertype,setUserType]=useState('')
     useEffect(() => {
         const auth = getAuth();
         const user = auth.currentUser;
-
+        const fetchUserId = async () => {
+            try {
+              const q = query(collection(db, 'users'), where('email', '==', user.email));
+              const querySnapshot = await getDocs(q);
+              if (!querySnapshot.empty) {
+                const userDoc = querySnapshot.docs[0];
+                const userData = userDoc.data();
+                setUserType(userData.type); // use `_id` to get the user ID from the data
+                console.log('User type:', usertype); // log the type of the user document
+              }
+            }catch (error) {
+                console.error('Error fetching user ID:', error);
+              } }
+        fetchUserId();
         if (user) {
             setUserEmail(user.email);
         } else {
@@ -20,10 +35,10 @@ export function Home() {
     }, []);
 
     const handleNavigate = (path,data) => {
-        if (userEmail&&data=='poc') {
+        if (userEmail&&data=='poc'&&usertype=='loc') {
             navigate("/PocDashboard");
         } 
-        else if(userEmail&&data=='donor'){
+        else if(userEmail&&data=='donor'&&usertype=='donor'){
             navigate("/donor")
         }
         
